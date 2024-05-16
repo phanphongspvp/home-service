38e3fe7bc7002634329b87a99a0afbfcc055ecdf
phongpv2 <phongpv2@bap.jp>
2024-05-16 09:57:23 +0700

    update 16/5 new

--- App/Screen/BusinessDetailsScreen/BookingModal.jsx
+++ App/Screen/BusinessDetailsScreen/BookingModal.jsx
@@ -1,8 +1,39 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, FlatList } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import CalendarPicker from "react-native-calendar-picker";
+import Color from "../../Utils/Color";
+import Heading from "../../Components/Heading";
 
 export default function BookingModal({ hiddenModal }) {
+
+  const [timeList, setTimeList] = useState(null);
+
+  useEffect(() => {
+    getTime();
+  }, []);
+
+  const getTime = () => {
+    const timeList = [];
+    for(let i = 8;i <= 12;i++) {
+      timeList.push({
+        time: i + ':00 AM'
+      })
+      timeList.push({
+        time: i + ":30 AM"
+      })
+    }
+    for(let i = 1;i <= 7;i++) {
+      timeList.push({
+        time: i + ':00 PM'
+      })
+      timeList.push({
+        time: i + ":30 PM"
+      })
+    }
+    setTimeList(timeList);
+  }
+
   return (
     <View style={{ padding: 20 }}>
       <TouchableOpacity
@@ -11,6 +42,7 @@ export default function BookingModal({ hiddenModal }) {
           flexDirection: "row",
           alignItems: "center",
           gap: 10,
+          marginBottom: 20
         }}
         onPress={() => hiddenModal()}
       >
@@ -19,6 +51,50 @@ export default function BookingModal({ hiddenModal }) {
           Đặt trước
         </Text>
       </TouchableOpacity>
+
+      {/* Calender Section */}
+      <Heading text={"Chọn ngày"} />
+      <View style={styles.calenderContainer}>
+        <CalendarPicker
+          onDateChange={this.onDateChange}
+          width={340}
+          minDate={Date.now()}
+          todayBackgroundColor={Color.BLACK}
+          todayTextStyle={{ color: Color.WHITE }}
+          selectedDayColor={Color.PRIMARY}
+          selectedDayTextColor={Color.WHITE}
+        />
+      </View>
+
+      {/* Time Select Section */}
+      <View>
+        <FlatList
+          data={timeList}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({item, index}) => (
+            <TouchableOpacity style={styles.selectedTime}>
+              <Text style={styles.unSelectedTime}>{item.time}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  calenderContainer: {
+    backgroundColor: Color.PRIMARY_LIGHT,
+    padding: 20,
+    borderRadius: 15
+  },
+  selectedTime: {
+
+  },
+  unSelectedTime: {
+    padding: 5,
+    borderWidth: 1,
+    borderColor: Color.PRIMARY
+  }
+})
